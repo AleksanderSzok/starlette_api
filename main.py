620f0bb0939e9aa9bb357a2b16ab9ee4fbfdd939
AleksanderSzok <szokmaster@gmail.com>
2023-02-05 23:43:33 +0100

    added to_xml endpoint

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+import asyncio
+
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, Response
 from starlette.routing import Route
+
+from json_to_xml_converter import JsonToXml
 
 
 async def homepage(request):
-    return JSONResponse({'hello': 'world'})
+    return JSONResponse({"hello": "world"})
 
 
-async def json_to_xml(request):
-    pass
+async def receive_xml_from_json(request):
+    data = await request.json()
+    json_xml = JsonToXml(json_string=data)
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(None, json_xml.json_to_xml)
+    return Response(content=str(json_xml), media_type="application/xml")
 
 
-app = Starlette(debug=True, routes=[
-    Route('/', homepage),
-    Route('/to_xml', json_to_xml, methods=["POST"])
-])
+app = Starlette(
+    debug=True,
+    routes=[
+        Route("/", homepage),
+        Route("/to_xml", receive_xml_from_json, methods=["POST"]),
+    ],
+)
